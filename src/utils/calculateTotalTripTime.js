76c53603906a2d6c4755ca59600d3d851524023c
luky1300 <58443403+luky1300@users.noreply.github.com>
2020-08-11 12:28:27 -0400

    tests for calculateTotalTripTime added, function refactored

--- src/utils/calculateTotalTripTime.js
+++ src/utils/calculateTotalTripTime.js
@@ -1,6 +1,7 @@
 export function diff_hours(dts2, dts1) {
     let dt1 = new Date(dts1);
     let dt2 = new Date(dts2);
+    if (dt1 > dt2) return 0
     let diff = (dt2.getTime() - dt1.getTime()) / 1000;
     diff /= 60;
     return Math.abs(Math.round(diff));
